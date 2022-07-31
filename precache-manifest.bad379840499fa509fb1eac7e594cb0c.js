3a4351e05a393c742cd632342a80c9b9f66bf11c
helenegmnt <98903316+helenegmnt@users.noreply.github.com>
2022-07-31 21:22:51 +0200

    Updates

---
+++ precache-manifest.bad379840499fa509fb1eac7e594cb0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d990bea96195086d49e0",
+    "url": "/static/css/main.40c16ed4.chunk.css"
+  },
+  {
+    "revision": "d990bea96195086d49e0",
+    "url": "/static/js/main.170b170e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3bfe8da54a4f8b79640f",
+    "url": "/static/js/2.dd40e78f.chunk.js"
+  },
+  {
+    "revision": "6561cd7d394bfc302c9659b968b585b6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/helenegmnt/helenegmnt.github.io/precache-manifest.a43e31ef86ab7bb5323f692f1468f427.js"
+  "/precache-manifest.bad379840499fa509fb1eac7e594cb0c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/helenegmnt/helenegmnt.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
